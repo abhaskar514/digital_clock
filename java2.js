fa1211afc457f7c81563ee345bf4abbd298061c0
abhaskar514 <100030867+abhaskar514@users.noreply.github.com>
2022-11-09 16:47:22 +0530

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,22 @@
+setInterval(displayTime, 1000);
+
+function displayTime() {
+
+    const timeNow = new Date();
+
+    let hoursOfDay = timeNow.getHours();
+    let minutes = timeNow.getMinutes();
+    let seconds = timeNow.getSeconds();
+ 
+    
+
+    hoursOfDay = hoursOfDay < 10 ? "0" + hoursOfDay : hoursOfDay;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let time = hoursOfDay + ":" + minutes + ":" + seconds;
+
+    document.getElementById('Clock').innerHTML =time ;
+
+}
+displayTime();
